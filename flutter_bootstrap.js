baddc62c4b5fa8044d10d792a4ee98d87147d7cd
Afsar Ali <155624077+afsarr05@users.noreply.github.com>
2025-01-11 10:18:26 +0500

    1.0.3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1803018192"
+    serviceWorkerVersion: "204246436"
   }
 });
